105297b24fb8cbbd7aaac77d1f165fa89e361355
张文广 <zhangwenguang@junzhengloan.com>
2019-05-29 18:08:15 +0800

    ports

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,21 +27,45 @@ export default {
       let index = 0;
       for (let start in this.graphData) {
         let allElements = this.graph.getElements();
-        let rect = new window.joint.shapes.standard.Rectangle();
-        let nodeExists = false;
-        rect.position(index * 200 + 100, (index + 1) * 100);
-        rect.resize(100, 40);
-        rect.attr({
-          body: {
-            fill: 'blue'
-          },
-          label: {
-            text: start,
-              fill: 'white'
+        let rect = new window.joint.shapes.devs.Model({
+          position: { x: index * 200 + 100, y: (index + 1) * 100 },
+          size: { width: 100, height: 40 },
+          attrs: { rect: { fill: 'blue' }, '.label': { text: start, fill: 'white'} },
+          inPorts: ['in1'],
+          outPorts: ['out'],
+          ports: {
+            groups: {
+              'in': {
+                attrs: {
+                  '.port-body': {
+                    fill: 'yellow'
+                  }
+                }
+              },
+              'out': {
+                attrs: {
+                  '.port-body': {
+                    fill: 'green'
+                  }
+                }
+              }
+            }
           }
         });
+        let nodeExists = false;
+        // rect.position(index * 200 + 100, (index + 1) * 100);
+        // rect.resize(100, 40);
+        // rect.attr({
+        //   body: {
+        //     fill: 'blue'
+        //   },
+        //   label: {
+        //     text: start,
+        //       fill: 'white'
+        //   }
+        // });
         for (let element of allElements) {
-          if (start === element.attributes.attrs.label.text) {
+          if (start === element.attributes.attrs['.label'].text) {
             rect = element;
             nodeExists = true;
             break;
@@ -53,9 +77,9 @@ export default {
         nodeExists = false;
         let rect2 = rect.clone();
         rect2.translate(200, 0);
-        rect2.attr('label/text', this.graphData[start][0]);
+        rect2.attr('.label/text', this.graphData[start][0]);
         for (let element of allElements) {
-          if (this.graphData[start][0] === element.attributes.attrs.label.text) {
+          if (this.graphData[start][0] === element.attributes.attrs['.label'].text) {
             rect2 = element;
             nodeExists = true;
             break;
@@ -65,9 +89,19 @@ export default {
           rect2.addTo(this.graph);
         }
 
-        let link = new window.joint.shapes.standard.Link();
-        link.source(rect);
-        link.target(rect2);
+        let link = new window.joint.shapes.uml.Transition();
+        link.source(rect.outPorts);
+        link.target(rect2.inPorts);
+        link.attr({
+              '.uml-state-body': {
+                  fill: 'rgba(48, 208, 198, 0.1)',
+                  stroke: 'rgba(48, 208, 198, 0.5)',
+                  'stroke-width': 1.5
+              },
+              '.uml-state-separator': {
+                  stroke: 'rgba(48, 208, 198, 0.4)'
+              }
+          }) 
         link.addTo(this.graph);
         index++;
       }
@@ -78,10 +112,26 @@ export default {
         el: '#joint',
         model: this.graph,
         width: '100%',
-        gridSize: 10,
+        gridSize: 1,
         drawGrid: true,
         background: {
             color: 'rgba(225, 225, 225, 0.9)'
+        },
+        defaultLink: new window.joint.dia.Link({
+          attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' } }
+        }),
+        validateConnection: function(cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
+          // Prevent linking from input ports.
+          if (magnetS && magnetS.getAttribute('port-group') === 'in') return false;
+          // Prevent linking from output ports to input ports within one element.
+          if (cellViewS === cellViewT) return false;
+          // Prevent linking to input ports.
+          return magnetT && magnetT.getAttribute('port-group') === 'in';
+        },
+        validateMagnet: function(cellView, magnet) {
+          // Note that this is the default behaviour. Just showing it here for reference.
+          // Disable linking interaction for magnets marked as passive (see below `.inPorts circle`).
+          return magnet.getAttribute('magnet') !== 'passive';
         }
       });
     this.draw();
@@ -100,13 +150,14 @@ export default {
       });
       rect.addTo(this.model);
     });
-    this.paper.on('element:pointerclick', function(cellView, eventObject, eventX, eventY) {
-      console.log('')
-    });
-    this.graph.on('change:position', function(cell) {
-      var center = cell.getBBox().center();
-      var label = center.toString();
-      cell.attr('label/text', label);
+
+    this.paper.on('element:pointerclick', function(view, event, x, y) {
+      let link = new window.joint.shapes.uml.Transition({
+        fill: 'rgba(48, 208, 198, 0.1)',
+        source: { id: view.model.id },
+        target: { x: x + 100, y: y + 100 }
+      });
+      link.addTo(this.model);
     });
   },
   props: {
@@ -117,4 +168,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "../../node_modules/jointjs/dist/joint.min.css";
 </style>
